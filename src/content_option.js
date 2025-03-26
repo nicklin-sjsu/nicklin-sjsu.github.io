a6487b52c399956068520a4f5053d9c9bafb7505
SuzyCheung <Joilsuzy@163.com>
2025-03-25 23:54:53 -0400

    Suzy Portfolio

---
+++ src/content_option.js
@@ -0,0 +1,167 @@
+const logotext = "SUZY";
+const meta = {
+    title: "Suzy Zhang",
+    description: "I’m Suzy Zhang Architect & Artist, currently studying in US",
+};
+
+const introdata = {
+    title: "I’m Suzy Zhang",
+    animated: {
+        first: "I love designing",
+        second: "I devolop cool websites",
+        third: "I build amazing projects",
+    },
+    description: "Hope that you will uncover the suble nuances and detailed thoughts concealed wihin every nookand cranny of my architectural creations.",
+    your_img_url: "https://i.imgur.com/12OU9S3.jpeg",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.imgur.com/yH52IeZ.png",
+        description: "SHARED RESIDENCE —— Continuous corridors activate living spaces",
+        link: "#",
+    },
+    {
+        img: "https://i.imgur.com/JxW6NNu.png",
+        description: "BAMBOO HUB —— Revitalization of seaweed production chain",
+        link: "#",
+    },
+    {
+        img: "https://i.imgur.com/aGaZePR.jpeg",
+        description: "SLOPE-SKYLINE TOD —— Combining traditional local culture with technological bus station",
+        link: "#",
+    },
+    {
+        img: "https://i.imgur.com/UB37KuC.jpeg",
+        description: "RISING TOGETHER —— A MODULAR FUTURE FOR GULFPORT ARTISTS",
+        link: "#",
+    },
+    {
+        img: "https://i.imgur.com/ogYkRT0.png",
+        description: "Reimagining Tradition —— Architectural Design Innovation: Impact Aesthetics",
+        link: "#",
+    },
+    {
+        img: "https://i.imgur.com/iAbtMVV.png",
+        description: "WEATHER-CASTED MSB —— Learning from Weakness, Chaos and Deficiency",
+        link: "#",
+    },
+    {
+        img: "https://i.imgur.com/yyRf854.png",
+        description: "Machina Matrix —— Autonomous Architecture for Secure Archival Efficiency.",
+        link: "#",
+    },
+ 
+    {
+        img: "https://i.imgur.com/hjQdlV1.png",
+        description: "BRICK PATH —— A space to recreate social boundary",
+        link: "#",
+    },
+    {
+        img: "https://i.imgur.com/mG6FwW6.png",
+        description: "A TRANQUIL SHELL —— A soft and heartfelt residence in concrete cities",
+        link: "#",
+    },
+    {
+        img: "https://i.imgur.com/DFgRB5F.png",
+        description: "BRICK HOTEL —— Uncover a colorful corner",
+        link: "#",
+    },
+    {
+        img: "https://i.imgur.com/adFqI4k.png",
+        description: "PATHWAYS OF TRANQUILITY —— Translating Traditional Lingnan Courtyards Through Spatial Roaming",
+        link: "#",
+    },
+   
+    {
+        img: "https://i.imgur.com/E6PEsez.png",
+        description: "CLARITY TRAIL —— Reconstruction of Local Culture and Materials",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "suzycheu@upenn.edu",
+    YOUR_FONE: "(267)292-1638",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
