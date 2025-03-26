f66fd9ea598c55b8f46c0c7aee65b1b8492e915d
nicklin <166683217+nicklin-finquantum@users.noreply.github.com>
2025-03-25 21:18:13 -0700

    Updates

---
+++ precache-manifest.4de32be0aa6e27442efc3c3f961da368.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14df978438997726b042bb3cda55c002",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6910f5d73ee1812dd042",
+    "url": "/static/css/2.67a1c4a1.chunk.css"
+  },
+  {
+    "revision": "f9305a9a7a9f6adc3eb0",
+    "url": "/static/css/main.be294073.chunk.css"
+  },
+  {
+    "revision": "6910f5d73ee1812dd042",
+    "url": "/static/js/2.72ea4a7d.chunk.js"
+  },
+  {
+    "revision": "f9305a9a7a9f6adc3eb0",
+    "url": "/static/js/main.640fca65.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
